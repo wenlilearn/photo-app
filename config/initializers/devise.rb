5d651ccb614123d0b93cd01cb0d2b5490d23d6a5
Wen Li <wenlilearn@gmail.com>
2020-02-24 21:56:25 -0500

    Upgrade mailer sender settings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'noreply@photo-app-wli.herokuapp.com'
+  config.mailer_sender = 'app162841705@heroku.com
+  '
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
